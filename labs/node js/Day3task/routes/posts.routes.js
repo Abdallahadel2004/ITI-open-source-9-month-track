b5b5050b2c7099b2355f159bcf5c6a50c80eff68
Abdallahadel2004 <abdo96650243@gmail.com>
2026-02-08 13:27:20 +0200

    remove some comments from the code

--- labs/node js/Day3task/routes/posts.routes.js
+++ labs/node js/Day3task/routes/posts.routes.js
@@ -5,10 +5,9 @@ import verifyToken from "../../Middleware/verfiyToken.js";
 import { Validation } from "../../Middleware/Validation.js";
 postsRoutes.use(verifyToken);
 
-//postsRoutes.get("/posts",getPosts);
 postsRoutes.get("/posts",getPosts);
 postsRoutes.post("/posts",Validation,createPosts);
 postsRoutes.put("/posts/:id",updatePosts);
 postsRoutes.delete("/posts/:id",deletePosts);
-//postsRoutes.get("/posts/:id",getPostById);
+
 export default postsRoutes;
--- labs/node js/Middleware/CatchErrors.js
+++ labs/node js/Middleware/CatchErrors.js
@@ -1,13 +1,11 @@
-// Higher-order function to wrap async route handlers
 export function catchErrors(fn) {
     return (req, res, next) => {
         fn(req, res, next).catch((err) => {
-            next(err); // Forward error to the global error-handling middleware
+            next(err); // Forward error to the global error
         });
     };
 }
 
-// Global error-handling middleware (register this LAST in app.use())
 export function globalErrorHandler(err, req, res, next) {
     const statusCode = err.statusCode || 500;
     res.status(statusCode).json({
