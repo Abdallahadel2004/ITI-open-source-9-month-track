ea6b6fe8fefe4fc958e75639079d509945982883
Abdallahadel2004 <abdo96650243@gmail.com>
2026-02-08 13:40:56 +0200

    remove some validation from the code

--- labs/node js/Day3task/routes/posts.routes.js
+++ labs/node js/Day3task/routes/posts.routes.js
@@ -2,11 +2,11 @@ import express from "express";
 const postsRoutes=express.Router();
 import { getPosts, createPosts, updatePosts, deletePosts } from "../controllers/posts.controller.js";
 import verifyToken from "../../Middleware/verfiyToken.js";
-import { Validation } from "../../Middleware/Validation.js";
+
 postsRoutes.use(verifyToken);
 
 postsRoutes.get("/posts",getPosts);
-postsRoutes.post("/posts",Validation,createPosts);
+postsRoutes.post("/posts",createPosts);
 postsRoutes.put("/posts/:id",updatePosts);
 postsRoutes.delete("/posts/:id",deletePosts);
 
--- labs/node js/Day3task/validations/UserValidation.js
+++ labs/node js/Day3task/validations/UserValidation.js
@@ -3,9 +3,7 @@ import joi from "joi";
 const userValidation=joi.object({
     name:joi.string().required().min(3).max(30),
     email:joi.string().email().required(),
-    password:joi.string().required().min(2).max(30),
-    post:joi.string().required().min(3).max(30),
-    comment:joi.string().required().min(3).max(30),
+    password:joi.string().required().min(2).max(30)
 });
 
 export default userValidation;
